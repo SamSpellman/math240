57b9db56ad060ee8651c27e59a5364c89c34a6ed
SamSpellman <sspellman2@wisc.edu>
2022-07-25 18:43:29 -0500

    Added abstract methods to SeqABC.py

--- week5/Sequences/SeqABC.py
+++ week5/Sequences/SeqABC.py
@@ -26,3 +26,23 @@ class SeqABC(ABC):
     def get(self, index):
         pass
 
+    """ Iterate over the sequence to determine if it is increasing
+        or decreasing. If two indexes are equal, then it can be neither
+        increasing or decreasing (by definition)
+    """
+    @abstractmethod
+    def IncrOrDecr(self):
+        pass
+
+    """ Call IncrOrDecr at the start of this method. This will determine
+        which conditionals need to be tested. If it is increasing, then it is
+        non-decreasing by definition. The opposite holds true for a decreasing
+        sequence. If it is neither decreasing or increasing, then you must
+        iterate over the sequence.
+        CAREFUL: a trend of non-decreasing/increasing must be true for the 
+                entire sequence. This means iterating over the whole array
+                is a requirement.
+    """
+    @abstractmethod
+    def nonDecrOrnonIncr(self)
+        pass
